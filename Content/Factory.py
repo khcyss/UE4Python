4ee963a719214f71b77da27ce30fb29f29e9c99a
khcys <1018470746@qq.com>
2021-04-08 09:05:44 +0800

    提交改动

--- Content/Factory.py
+++ Content/Factory.py
@@ -1,9 +1,8 @@
 import unreal_engine as ue
 from unreal_engine.classes import BlueprintPathsLibrary
 import os
-from unreal_engine import FbxImporter, FbxMesh, FbxNode, FbxScene, FbxObject
-from unreal_engine.classes import FbxImportUI, StaticMesh, Class, Object
-from unreal_engine.classes import PyFactory
+from unreal_engine import FbxImporter, FbxMesh, FbxNode, FbxScene, FbxObject, FbxManager, FbxIOSettings
+from unreal_engine.classes import FbxImportUI, StaticMesh, Class, Object, PyFactory
 
 
 class MyFbxFactory(PyFactory):
@@ -13,10 +12,26 @@ class MyFbxFactory(PyFactory):
         self.Formats = ['fbx;FBX']
         self.SupportedClass = StaticMesh
         self.ImportPriority = 120
+        self.manager = FbxManager()
+        self.io_settings = FbxIOSettings(self.manager, 'IOSROOT')
+        self.manager.set_io_settings(self.io_settings)
+        self.importer = FbxImporter(self.manager, 'importer')
+        self.scene = FbxScene(self.manager, 'scene')
 
-    def PyFactoryCreateFile(self, class_: Class, parent: Object, name: str, filename: str) -> Object:
-        newimporter = FbxImporter()
-        newimporter.initialize('C:/Users/VR/Desktop/112412.FBX')
+    def get_objects_by_class(self, name):
+        objects = []
+        for i in range(0, self.scene.get_src_object_count()):
+            obj = self.scene.get_src_object(i)
+            if obj.get_class_name() == name:
+                objects.append(obj)
+        return objects
+
+    def PyFactoryCreateFile(self, parent: Object, name: str, filename: str) -> Object:
+        self.importer.initialize(filename, self.io_settings)
+        self.importer._import(self.scene)
+        tempffbx = self.get_objects_by_class('FbxImporter')
+        tempinstance = tempffbx._get_instance()
+        tempinstance._import_from_file(filename, 'fbx', True)
         return None
 
     def ScriptFactoryCanImport(self, filename: str) -> bool:
